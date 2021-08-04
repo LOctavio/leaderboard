cd6ccf0d206f1e27c2dce8b23178c532a8c3664c
LOctavio <luis_ram1997@hotmail.com>
2021-08-04 12:40:49 -0500

    Add function for creating the game and get its Id

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './style.css';
+
+async function getGameID() {
+  const gameId = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', { 
+  method: 'POST',
+  body: JSON.stringify({ name: 'my new game' }),
+  headers:{
+    'Content-Type': 'application/json'
+  },
+})
+  .then((response) => response.json())
+  .then((json) =>  { 
+    const result = json.result;
+    const gameId = result.replace('Game with ID: ', '').replace(' added.','');
+    return gameId; 
+   });
+  }
