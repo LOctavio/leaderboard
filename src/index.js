979bdd31938948cbb2698ee139e8dfbcc5d67411
LOctavio <luis_ram1997@hotmail.com>
2021-08-04 14:56:15 -0500

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -5,36 +5,33 @@ function getGameID(gameId) {
 }
 
 async function setGameName(getGameID) {
-  const gameId = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', { 
+  await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
     method: 'POST',
     body: JSON.stringify({ name: 'my new game' }),
-    headers:{
-      'Content-Type': 'application/json'
+    headers: {
+      'Content-Type': 'application/json',
     },
   })
-  .then((response) => response.json())
-  .then((json) =>  
-  { 
-    const result = json.result;
-    const gameId = result.replace('Game with ID: ', '').replace(' added.','');
-    getGameID(gameId); 
-  });
+    .then((response) => response.json())
+    .then((json) => {
+      const { result } = json;
+      const gameId = result.replace('Game with ID: ', '').replace(' added.', '');
+      getGameID(gameId);
+    });
 }
 
-
 async function creteNewScore(user, score, getGameID) {
-  const gameId = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/'+ getGameID +'/scores/', { 
+  await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${getGameID}/scores/`, {
     method: 'POST',
-    body: JSON.stringify({ 
-      user: user,
-      score: score,
+    body: JSON.stringify({
+      user,
+      score,
     }),
-    headers:{
-      'Content-Type': 'application/json'
+    headers: {
+      'Content-Type': 'application/json',
     },
   })
-  .then((response) => response.json())
-  .then((json) =>  console.log(json));
+    .then((response) => response.json());
 }
 
 document.querySelector('.submit-button').addEventListener('click', () => {
@@ -42,18 +39,19 @@ document.querySelector('.submit-button').addEventListener('click', () => {
   const score = document.querySelector('.score').value;
   creteNewScore(user, score, getGameID);
 });
- 
+
 function printScoresList() {
-  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/'+ getGameID +'/scores/')
-  .then(response => response.json())
-  .then(json => {
-    const container = document.querySelector('.scores-list ul');
-    json.result.forEach(element => {
-      const score = document.createElement('li');
-      score.innerHTML = element.user + ': ' + element.score;
-      container.appendChild(score);
+  const container = document.querySelector('.scores-list ul');
+  fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${getGameID}/scores/`)
+    .then((response) => response.json())
+    .then((json) => {
+      container.innerHTML = '';
+      json.result.forEach((element) => {
+        const score = document.createElement('li');
+        score.innerHTML = `${element.user}: ${element.score}`;
+        container.appendChild(score);
+      });
     });
-  });
 }
 
 document.querySelector('.refresh').addEventListener('click', () => {
