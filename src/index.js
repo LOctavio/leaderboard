0f14c613ef06125b8169319cccfdbeb021598426
LOctavio <luis_ram1997@hotmail.com>
2021-08-04 13:36:55 -0500

    Use callback for getting the game Id

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import './style.css';
 
-async function getGameID() {
+function getGameID(gameId) {
+  return gameId;
+}
+
+async function setGameName(getGameId) {
   const gameId = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', { 
-  method: 'POST',
-  body: JSON.stringify({ name: 'my new game' }),
-  headers:{
-    'Content-Type': 'application/json'
-  },
-})
+    method: 'POST',
+    body: JSON.stringify({ name: 'my new game' }),
+    headers:{
+      'Content-Type': 'application/json'
+    },
+  })
   .then((response) => response.json())
-  .then((json) =>  { 
+  .then((json) =>  
+  { 
     const result = json.result;
     const gameId = result.replace('Game with ID: ', '').replace(' added.','');
-    return gameId; 
-   });
-  }
+    getGameID(gameId); 
+  });
+}
